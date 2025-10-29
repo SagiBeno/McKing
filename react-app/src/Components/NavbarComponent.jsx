1a92c44ba3162a36e34e7eb74407a37e995bf218
SagiBeno <benosagi321@gmail.com>
2025-10-29 16:03:28 +0100

    Implement Navbar

--- react-app/src/Components/NavbarComponent.jsx
+++ react-app/src/Components/NavbarComponent.jsx
@@ -1,9 +1,12 @@
+import { Navbar, Nav, Container, NavLink } from "react-bootstrap"
+import { Link } from "react-router-dom";
+
 export default function NavbarComponent() {
     return (
         <>
             <Navbar bg="light" expand="lg" className="bg-body-tertiary" fixed="top">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand>
                         <img
                             src="/McKing.png"
                             width="30"
@@ -16,7 +19,8 @@ export default function NavbarComponent() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav className="me-auto">
-                          <Nav.Link as={Link} to="/order">Új rendelés leadása</Nav.Link>
+                          <Nav.Link href="/order">Új rendelés leadása</Nav.Link>
+                          <Nav.Link href="/orders">Rendelések</Nav.Link>
                         </Nav>
                         <Navbar.Text>
                             Bejelentkezve: <a href="/" className="logoutButton">Felhasználónév</a>
