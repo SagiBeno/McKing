939ad73b1b5c49347477f07681d727bde4dfa300
hajos8 <rabb.andras@gmail.com>
2025-10-29 14:34:38 +0100

    Outsourced mapping from OrderComponent to OrderPage

--- react-app/src/Pages/OrderPage.jsx
+++ react-app/src/Pages/OrderPage.jsx
@@ -1,20 +1,68 @@
 import { useEffect, useState } from 'react'
 import OrderComponent from '../Components/OrderComponent'
 import { useNavigate } from "react-router";
+import { Form, Row } from 'react-bootstrap';
+import { toast, ToastContainer } from 'react-toastify';
 
 export default function OrderPage() {
-    const [order, setOrder] = useState()
+    const [order, setOrder] = useState({})
+    const [foods, setFoods] = useState([])
     let navigate = useNavigate()
 
-    const handleOreder = data => {
-        setOrder([...data])
-        navigate('/status');
+    useEffect(() => {
+        fetch('http://localhost:3333/api/foods')
+        .then(async res =>{
+            const data = await res.json()
+
+            setFoods(data)
+            console.log(data)
+        })
+        .catch(err => {
+            console.log("Hiba az adatok lekérése során: ", err)
+        })
+    }, [])
+
+
+
+    const handleOrder = data => {
+        console.log(data)
+        //navigate('/status');
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        var ordered = []
+        order.map((element) => {
+            if (element.quantity != 0) {
+                ordered.push(element)
+            }
+        })
+
+        if (ordered.length > 0) {
+            props.onOrder(ordered)
+            toast.success('Rendelés leadása sikeresen megtörtént!')
+        } else {
+            toast.warning('Kérem válasszon ki ételt!');
+        }
     }
 
     return (
         <div>
             <h1>McKing - Menü</h1>
-            <OrderComponent onOrder={handleOreder}/> 
+
+            <Form onSubmit={handleSubmit}>
+                <Row>
+
+                    {
+                        foods.map( (element, key) => (
+                            <OrderComponent key={key} element={element} onOrderChange={handleOrder} />
+                        ))
+                    }
+                </Row>
+                <button type="submit" className="orderSubmitButton shadow">Rendelés leadása</button>
+            </Form>
+            <ToastContainer position="top-center"/>
         </div>
     )
 }
--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -7,16 +7,86 @@ const mysql = require('mysql2');
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
 /* TODO - use cors, json middleware */
 
 
 var users = [];
 var orders = [];
 
+const etelek = [
+        { 
+            id: 1, 
+            name: 'Hamburger', 
+            price: 1250, 
+            image: 'https://images.unsplash.com/photo-1550547660-d9450f859349?w=800'
+        },
+        { 
+            id: 2, 
+            name: 'CocaCola', 
+            price: 300, 
+            image: 'https://images.unsplash.com/photo-1667204651371-5d4a65b8b5a9?w=800'
+        },
+        { 
+            id: 3, 
+            name: 'Sajtburger', 
+            price: 1450, 
+            image: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=800'
+        },
+        { 
+            id: 4, 
+            name: 'Dupla Sajtburger', 
+            price: 1850, 
+            image: 'https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=800'
+        },
+        { 
+            id: 5, 
+            name: 'Hasábburgonya', 
+            price: 750, 
+            image: 'https://images.unsplash.com/photo-1571091718767-18b5b1457add?w=800'
+        },
+        { 
+            id: 6, 
+            name: 'Csirkefalatok', 
+            price: 990, 
+            image: 'https://images.unsplash.com/photo-1619881590738-a111d176d906?w=800'
+        },
+        { 
+            id: 7, 
+            name: 'Jégkrém', 
+            price: 600, 
+            image: 'https://images.unsplash.com/photo-1560008581-09826d1de69e?w=800'
+        },
+        { 
+            id: 8, 
+            name: 'Kávé', 
+            price: 450, 
+            image: 'https://images.unsplash.com/photo-1511920170033-f8396924c348?w=800'
+        },
+        { 
+            id: 9, 
+            name: 'Limonádé', 
+            price: 550, 
+            image: 'https://images.unsplash.com/photo-1623084921164-4a8c5c37a912?w=800'
+        },
+        { 
+            id: 10, 
+            name: 'Hot Dog', 
+            price: 990, 
+            image: 'https://images.unsplash.com/photo-1613482084286-41f25b486fa2?w=800'
+           }
+    ];
+
 /* TODO - introduce endpoints */
 
+app.get('/api/foods', (req, res) => {
+    res.json(etelek);
+});
 
-const port = 5000;
+
+const port = 3333;
 app.listen(port, () => {
   console.log(`Backend fut: http://localhost:${port}`);
 });
