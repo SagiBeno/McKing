0082d90113ad6bac83e5065a582862cf80a3131e
hajos8 <rabb.andras@gmail.com>
2025-10-31 22:38:34 +0100

    fixed routing bug

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
           <Route path="/" element={loggedIn ? <Navigate to="/order" /> : <LoginPage onLogin={handleLogin} />} />
           <Route path="/register" element={loggedIn ? <Navigate to="/order" /> : <RegistrationPage onRegister={handleLogin} />} /> {/* Login after registration */}
           <Route path='/order' element={<OrderPage username={username} />} />
-          <Route path='/orders' element={<OrdersPage />} />
+          <Route path='/current-orders' element={<OrdersPage />} />
           <Route path='/status' element={<StatusPage username={username} />} />
         </Routes>
       </Container>
