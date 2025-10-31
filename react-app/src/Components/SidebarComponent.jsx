21b8a31e6a3bb9cbfc355296b594224da86869e0
SagiBeno <benosagi321@gmail.com>
2025-10-31 14:23:27 +0100

    Add icons

--- react-app/src/Components/SidebarComponent.jsx
+++ react-app/src/Components/SidebarComponent.jsx
@@ -28,11 +28,11 @@ export default function SidebarComponent(props) {
             <div className="sidebar" style={{width: `${sidebarWidth}px`}}>
                 <span id="sidebarCloseButton" onClick={close} style={{display: closeButton}}><i className="fa-solid fa-xmark fa-3x" style={{color: '#ab1700'}}></i></span>
                 <div className="sidebarContent">
-                    <button type="button" onClick={filter} value='Szendvicsek'>Szendvicsek</button>
-                    <button type="button" onClick={filter} value='Italok'>Italok</button>
-                    <button type="button" onClick={filter} value='Desszertek'>Desszertek</button>
-                    <button type="button" onClick={filter} value='Köretek'>Köretek</button>
-                    <button type="button" onClick={filter} value="Harapnivalók">Harapnivalók</button>
+                    <button type="button" onClick={filter} value='Szendvicsek'><i className="fa-solid fa-burger fa-lg"></i> Szendvicsek</button>
+                    <button type="button" onClick={filter} value='Italok'><i className="fa-solid fa-mug-saucer fa-lg"></i> Italok</button>
+                    <button type="button" onClick={filter} value='Desszertek'><i className="fa-solid fa-ice-cream fa-lg"></i> Desszertek</button>
+                    <button type="button" onClick={filter} value='Köretek'><i className="fa-solid fa-plate-wheat fa-lg"></i> Köretek</button>
+                    <button type="button" onClick={filter} value="Harapnivalók"><i className="fa-solid fa-drumstick-bite fa-lg"></i> Harapnivalók</button>
                 </div>
                 <span id="sidebarOpenButton" onClick={open} style={{display: openButton}}><i className="fa-solid fa-magnifying-glass fa-2x" style={{color: '#ab1700'}}></i></span>
             </div>
