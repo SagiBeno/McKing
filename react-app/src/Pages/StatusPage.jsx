9a0bef590d36e7898da8e877c65846268c72d27f
SagiBeno <benosagi321@gmail.com>
2025-10-31 14:11:42 +0100

    Add Spinner

--- react-app/src/Pages/StatusPage.jsx
+++ react-app/src/Pages/StatusPage.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
+import Spinner from '../Components/Spinner';
 
 export default function StatusPage () {
     const location = useLocation();
     const orderId = location.state.orderId >= 0 ? location.state.orderId : -1;
     const [order, setOrder] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         if (orderId === -1) {
             return;
         }
@@ -19,24 +22,24 @@ export default function StatusPage () {
         .catch(err => {
             console.log("Hiba az adatok lekérése során: ", err)
         })
+        .finally(load => setIsLoading(false))
     }, []);
 
     return(
         <>
             <h1>Rendelésed:</h1>
-            <Card>
-                <Card.Header>Rendelés azonosító: {orderId}</Card.Header>
+            <Card className='shadow'>
+                <Card.Header>Rendelés azonosítója: {orderId}</Card.Header>
                 <Card.Body>
                     {Object.values(order).map((item, idx) => (
                         <p key={idx}>
-                            {item.name} - {item.quantity}db
+                            {item.name} - {item.quantity} db
                         </p>
                     ))}
                 </Card.Body>
-                <Card.Footer>Rendelésed állapota: </Card.Footer> 
-                            
+                <Card.Footer>Rendelésed állapota: </Card.Footer>         
             </Card>
-
+            {isLoading && <Spinner />}
         </>
     )
 }
